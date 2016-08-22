267c700ed51ed5f40cbeb906500ab230698ac9ca
Aftab Ansari <aftabansari10662@gmail.com>
2016-08-22 07:56:36 +0530

    Inital Commit

---
+++ MethodHidingVsMethodOverriding/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodHidingVsMethodOverriding
+{
+    public class Employee
+    {
+
+        public void GetFullName()
+        {
+            Console.WriteLine("I am a base class method.");
+        }
+    }
+
+    public class PermanentEmployee:Employee
+    {
+
+        public new void GetFullName()
+        {
+            Console.WriteLine("I am a child class method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee e = new PermanentEmployee();
+
+            e.GetFullName();
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+
+    public abstract class Employee
+    {
+
+        private string firstName = "Aftab";
+        private string lastName = "Ansari";
+
+        public void GetFullName()
+        {
+            Console.WriteLine($"{firstName} {lastName}");
+        }
+
+    }
+
+    public class PermanentEmployee : Employee
+    {
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new PermanentEmployee();
+            e.GetFullName();
+
+            Console.ReadLine();
+        }
+    }
+}
