9f76aca92975f9761ad0214c15f94871125c9bf6
Aftab Ansari <aftabansari10662@gmail.com>
2016-08-25 20:26:33 +0530

    Extension Method

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var text = "36.24";
+
+            var parsedValue = text.ToDouble();
+            Console.WriteLine($"value: {parsedValue}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExtensionMethod/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class StringExtension
+    {
+
+        public static double ToDouble(this string input)
+        {
+            double result = double.Parse(input);
+
+            return result;
+        }
+    }
+}
