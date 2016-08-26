621a988ee428f894d7a3de40a1ef23b64d18f041
Aftab Ansari <aftabansari10662@gmail.com>
2016-08-26 21:27:54 +0530

    Sinleton Design Pattern

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+
+    public class Foo
+    {
+        private Foo()
+        {
+
+        }
+
+        public int X { get; set; }
+        private static Foo FooInstance { get; set; }
+
+        public static Foo GetInstance()
+        {
+            if (FooInstance == null)
+            {
+                FooInstance = new Foo();
+            }
+
+            return FooInstance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var foo1 = Foo.GetInstance();
+            foo1.X = 20;
+            var foo2 = Foo.GetInstance();
+
+            Console.WriteLine(foo1.X);
+            Console.WriteLine(foo2.X);
+            Console.ReadLine();
+        }
+    }
+}
